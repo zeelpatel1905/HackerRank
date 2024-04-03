b25fb8081a8ee3ead3f8940fb32606f928e19c05
Zeel Patel <46553797+zeelpatel1905@users.noreply.github.com>
2024-04-03 15:47:47 +0530

    query of Weather Observation Station 9
    Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.

---
+++ SQL/Weather Observation Station 9.sql
@@ -0,0 +1,22 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+Input Format
+
+The STATION table is described as follows:
+
+============================
+| Field       |   Type     |
+============================
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
+============================
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*/
+
+SELECT DISTINCT city
+FROM station 
+WHERE lower(substr(CITY,1,1)) not in ('a', 'e', 'i', 'o', 'u');
