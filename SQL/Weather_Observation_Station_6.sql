90ea0fd9560d2680e687ebb5a02b7c724ad51a98
Zeel Patel <46553797+zeelpatel1905@users.noreply.github.com>
2024-04-03 15:16:58 +0530

    query of Weather_Observation_Station_6.sql
    Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
    Input Format

---
+++ SQL/Weather_Observation_Station_6.sql
@@ -0,0 +1,22 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+Input Format
+
+The STATION table is described as follows:
+
+============================
+| Field       |   Type     |
+============================
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
+============================
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*/
+
+SELECT DISTINCT city
+FROM station 
+WHERE lower(substr(CITY,1,1)) in ('a', 'e', 'i', 'o', 'u');
